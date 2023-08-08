4732e6235be330e0d191eb0a82ec560197771df1
Jesus <jesus.soto@idoagency.com>
2023-08-08 16:34:53 -0400

    Condicionales

---
+++ 3.4.Condicionales/script.js
@@ -0,0 +1,28 @@
+
+/* Condicionales */
+
+/* if else - if else if */
+
+/* if(edad2 > 17){
+    console.log("mi edad es " + edad2)
+
+}else{
+    console.log("Eres menor de edad, no puedes entrar a este burdel")
+} */
+
+if(names == 'ipo' && edad2 >= 17 && lastName == 'garlipo'){
+    console.log("Tu nombre es " + names)
+
+
+}else if(names == 'jesus'){
+        console.log("Tu nombre es " + names)
+
+}else if(names == 'manito'){
+        console.log("Tu nombre es " + names)
+
+}else if(names == 'yessi'){
+        console.log("Tu nombre es " + names)
+
+}else{
+    console.log("Eres menor de edad, no puedes entrar a este burdel")
+}
